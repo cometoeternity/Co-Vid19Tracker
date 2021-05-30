daa2968d16361866c47d275f9c2b4e22c92d8732
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-05-30 13:01:39 +0700

    Добавление тестового консольного проекта.

---
+++ Tests/Co-Vid19TrackerConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Co_Vid19TrackerConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
