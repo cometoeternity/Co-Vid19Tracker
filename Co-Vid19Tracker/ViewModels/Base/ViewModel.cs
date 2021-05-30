ad44971746b12d1d9201c9aa0fee7ac04e02415a
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-05-30 22:13:00 +0700

    Добавление базовой ViewModel и возможный вид реализаии интерфейса IDispose.

---
+++ Co-Vid19Tracker/ViewModels/Base/ViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Co_Vid19Tracker.ViewModels.Base
+{
+    internal abstract class ViewModel : INotifyPropertyChanged, IDisposable
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected virtual bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(field, value)) return false;
+            field = value;
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+
+        private bool _Disposed;
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing || _Disposed) return;
+            _Disposed = true;
+            //Освобождение управляемых ресурсов.
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+}
