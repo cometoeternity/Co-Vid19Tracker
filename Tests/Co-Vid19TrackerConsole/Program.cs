98ca2abb09affa538f3cf5ae58adc8c893ef744e
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-05-31 21:51:44 +0700

    Скачивание csv-файла с количеством зараженных на данный момент времени.

--- Tests/Co-Vid19TrackerConsole/Program.cs
+++ Tests/Co-Vid19TrackerConsole/Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 
 namespace Co_Vid19TrackerConsole
 {
     class Program
     {
+
+        private const string DATA_URL = @"https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Для старых версий.
+            //WebClient client = new WebClient();
+
+            var client = new HttpClient();
+            var response = client.GetAsync(DATA_URL).Result;
+            var csv_str = response.Content.ReadAsStringAsync().Result;
+
+            
+            Console.ReadLine();
         }
     }
 }
