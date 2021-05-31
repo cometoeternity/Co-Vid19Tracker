7b3aebe31c5e1b9106ef203780d6866993e2d242
Дмитрий Павлов <dmitriyeternitypavlov@outlook.com>
2021-05-31 19:19:19 +0700

    Добавление команды из отдельного класса команд.

---
+++ Co-Vid19Tracker/Infrastructure/Commands/CloseApplicationCommand.cs
@@ -0,0 +1,14 @@
+﻿using Co_Vid19Tracker.Infrastructure.Commands.Base;
+using System.Windows;
+
+namespace Co_Vid19Tracker.Infrastructure.Commands
+{
+    internal class CloseApplicationCommand : Command
+    {
+        public override bool CanExecute(object parameter) => true;
+
+
+        public override void Execute(object parameter) => Application.Current.Shutdown();
+        
+    }
+}
